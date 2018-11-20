00a6142e25ef16e97ef23e424d884e796bea3bc3
Ngoc Anh <ntngocanh21@gmail.com>
2018-11-20 09:31:42 +0700

    temp

--- app/src/main/java/com/senior/project/genealogy/view/fragment/search/SearchFragment/SearchFragment.java
+++ app/src/main/java/com/senior/project/genealogy/view/fragment/search/SearchFragment/SearchFragment.java
@@ -1,7 +1,9 @@
 package com.senior.project.genealogy.view.fragment.search.SearchFragment;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +11,17 @@ import android.view.ViewGroup;
 import com.senior.project.genealogy.R;
 import com.senior.project.genealogy.view.activity.home.HomeActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class SearchFragment extends Fragment {
+
+    @BindView(R.id.viewpager)
+    ViewPager mViewPager;
+
+    @BindView(R.id.tabs)
+    TabLayout mTabLayout;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -19,8 +30,10 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ((HomeActivity) getActivity()).updateTitleBar("Search");
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        ((HomeActivity) getActivity()).updateTitleBar(getString(R.string.frg_search));
+        ButterKnife.bind(this, view);
+        return view;
     }
 
 }
